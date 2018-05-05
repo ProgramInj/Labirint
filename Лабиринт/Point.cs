ed8028a5f3d06ce3c7f7d64fab1c5fb01d79d0a6
Adima <777adima@bk.ru>
2018-05-05 12:48:05 +0400

    class Point

---
+++ Лабиринт/Point.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Лабиринт
+{
+    class Point
+    {
+        public int dl { get; set; }
+        public int shir { get; set; }
+
+        public Point(int a, int b)
+        {
+            this.dl = a;
+            this.shir = b;
+        }
+
+        public void StrRight()
+        {
+            var tmp = this.dl;
+            this.dl = this.shir * -1;
+            this.shir = tmp;
+        }
+
+        public void StrLeft()
+        {
+            var tmp = this.dl;
+            this.dl = this.shir;
+            this.shir = tmp * -1;
+        }
+    }
+}
